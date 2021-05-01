0ec78b956c37f90c0eb5f7bfc6a885f553a86065
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-04-30 21:14:42 -0600

    Tarea Hashmap

---
+++ Tarea/src/tarea/Tarea.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea;
+
+import Vista.FramePrincipal;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Tarea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       FramePrincipal v1=new FramePrincipal();
+       v1.show();
+    }
+    
+}
